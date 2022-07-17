4aeffb277835b0e0ba08be4a5280ced5e8941f68
Frédéric Alphonse <101244166+Fred-23@users.noreply.github.com>
2022-07-17 17:30:49 +0000

    Test all the new functions and try to authentificate some message. Modify the sender to send message after some index (2)

--- main.c
+++ main.c
@@ -4,7 +4,18 @@
 #include "md5.h"
 #include "sender.h"
 
-uint8_t* Table_of_Keys[10];
+
+//TO DO
+/*
+Check the table of keys (init plus incrementation)
+and md5Pow function
+mac with the same message OK
+Do the receiver part as python but make something different for the index, maybe an interruption or an condition in parallel
+In a first time don't lose to much time on that because it will be use differently in the drone
+
+Test all the new functions and try to authentificate some message. Modify the sender to send message after some index (2)
+*/
+uint8_t* Table_of_Keys[10]={0};
 
 char data_raw[12]= "Hello World";
 char *Pointer = &data_raw[12];
@@ -23,6 +34,8 @@ int main(){
     //free(result);
     }
 
+  mac_result=mac(result,data_raw);
+  print_hash(mac_result);
   mac_result=mac(result,data_raw);
   print_hash(mac_result);
   //Need improvement
