eee30e0eebd9abdd2a594a34c89382a6c7fc9dd5
Frédéric Alphonse <101244166+Fred-23@users.noreply.github.com>
2022-07-18 13:38:35 +0000

    Clean version before hugh modifications

--- main.c
+++ main.c
@@ -6,12 +6,15 @@
 
 //TO DO
 /*
-Check the table of keys (init plus incrementation)
+Check the table of keys (init plus incrementation) OK
 and md5Pow function OK
 mac with the same message OK
+
 Do the receiver part as python but make something different for the index, maybe an interruption or an condition in parallel
-In a first time don't lose to much time on that because it will be use differently in the drone
+Not necessary since we have a node and a anchor
+
 Put a lot of comments for my tutor
+
 Test all the new functions and try to authentificate some message. Modify the sender to send message after some index (2)
 */
 
@@ -28,14 +31,20 @@ int main(){
   result = md5String(data_raw);
   resultpow=md5Pow(result,2);
   //print_hash(resultpow);
+
+  
   //-------------------Sender Part----------------------//
   for(int i=0; i<10;i++){
+    
 	  Table_of_Keys[i]=result;
+    
     printf("index %i   :", i);
 	  print_hash(result);
-    print_hash(Table_of_Keys[i]);
+    
     result = md5Number(result);
-
+      
+  //print_hash(Table_of_Keys[i]);*
+    
       /*struct Packet_Tesla
         {
            int index;
@@ -45,21 +54,12 @@ int main(){
         };*/  
     //--------Use packets structure----//
 
-    //free(result);
     }
-
-  mac_result=mac(result,data_raw);//Will change in stm32 so not a issue only not for the reicever
-  print_hash(mac_result);
-
-  
-  //mac_result=mac(result,);//Will change in stm32 so not a issue only not for the reicever
-  //print_hash(mac_result);
-  //Need improvement
-  /*char_result = md5Char(data_raw);
-  for(int i=0; i<10;i++){
-    printf("index %i   :", i);
-	  print_char_hash(char_result);
-    char_result = md5Char(char_result);
+    //To free-up the memory
     //free(result);
-    }*/
+  
+  mac_result=mac(result,data_raw);
+  //Will change in stm32F4
+  print_hash(mac_result);
+ 
 }
